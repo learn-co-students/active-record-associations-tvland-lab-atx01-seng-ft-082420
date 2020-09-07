c5f04f310566cd5ea1a8fe910b9800e4a1da642f
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-09-06 19:01:57 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors
+    belongs_to :network
+
+    def actors_list
+        Actor.all.collect do |actor|
+            actor.full_name
+        end
+    end
   
 end
